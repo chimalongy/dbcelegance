4dc78a7a136abc30e2265cdf22ce0e1975adf9ea
Chima Longy <me.chimaobi@gmail.com>
2025-08-10 22:26:56 +0100

    added databse url

--- utils/DB/DBConnect.js
+++ utils/DB/DBConnect.js
@@ -1,20 +1,29 @@
-import { Pool } from "pg";
+// import { Pool } from "pg";
+
+// // const pool = new Pool({
+// //   user: 'postgres',
+// //   host: 'localhost',
+// //   database: 'dbcelegance',
+// //   password: '1',
+// //   port: 5432,
+// // })
 
 // const pool = new Pool({
-//   user: 'postgres',
-//   host: 'localhost',
-//   database: 'dbcelegance',
-//   password: '1',
+
 //   port: 5432,
-// })
+//   host: "aws-0-eu-north-1.pooler.supabase.com",
+//   database: "postgres",
+//   user: "postgres.mwybgicbeqwphroaerpy",
+//   pool_mode: "session",
+//   password:"chimsyboy"
+// });
+// export default pool;
+
+import { Pool } from "pg";
 
 const pool = new Pool({
-
-  port: 5432,
-  host: "aws-0-eu-north-1.pooler.supabase.com",
-  database: "postgres",
-  user: "postgres.mwybgicbeqwphroaerpy",
-  pool_mode: "session",
-  password:"chimsyboy"
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // Supabase requires SSL
 });
+
 export default pool;
